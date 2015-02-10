ed944f91b012d815e7ac10978725729de7975a21
aldegtyarev <aldegtyarev@yandex.ru>
2015-02-10 17:38:40 +0300

    10.02.15. 17.38

--- themes/perevozkin/views/layouts/main.php
+++ themes/perevozkin/views/layouts/main.php
@@ -24,45 +24,102 @@ $theme_baseUrl = Yii::app()->theme->baseUrl;
 
 <body>
 
-<div class="container" id="page">
+<div class="wrapper">
+	<div class="wrapperPage">
+		<div class="header">
+			<div class="top-line">
+				<div class="width-wrap">
+					<div class="main-menu floatLeft">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+						<?php $this->widget('zii.widgets.CMenu',array(
+							'items'=>array(
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				//array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-				array('url'=>Yii::app()->getModule('user')->loginUrl, 'label'=>Yii::app()->getModule('user')->t("Login"), 'visible'=>Yii::app()->user->isGuest),
-				array('url'=>Yii::app()->getModule('user')->registrationUrl, 'label'=>Yii::app()->getModule('user')->t("Register"), 'visible'=>Yii::app()->user->isGuest),
-				array('url'=>Yii::app()->getModule('user')->profileUrl, 'label'=>Yii::app()->getModule('user')->t("Profile"), 'visible'=>!Yii::app()->user->isGuest),
-				array('url'=>Yii::app()->getModule('user')->logoutUrl, 'label'=>Yii::app()->getModule('user')->t("Logout").' ('.Yii::app()->user->name.')', 'visible'=>!Yii::app()->user->isGuest),				
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+								array('label'=>'Главная', 'url'=>array('/')),
+								array('label'=>'тест', 'url'=>array('/pages/view', 'id'=> 1)),
+								array('label'=>'Оплата и доставка', 'url'=>array('/site/oplataidostavka')),
+								array('label'=>'Контакты', 'url'=>array('/site/contact')),
+								//array('label'=>'Вход', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+								//array('label'=>'Выход ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+								
+								array('label'=>'Отзывы', 'url'=>array('/site/feedback'), 'itemOptions'=>array('class'=>'last-item'),),
+							),'htmlOptions' => array('class'=>'main-menu clearfix', 'id'=>'main-menu')
+						)); ?>
+					</div>
+					<?php //$this->widget('application.components.MsiLogin'); ?>
+					<?php //$this->widget('application.components.CartWidget'); ?>
+				</div>
+			</div>
+			
+			<div class="header-block width-wrap clearfix">
+				<div class="block1 floatLeft">
+					<a href="/" class="logo-top white-color uppercase font-size-13">
+						<img src="/img/logo-top.png" alt="магазин автомобильных запчастей" />
+						магазин автомобильных запчастей
+					</a>
+					<?php $this->widget('application.components.SearchWidget'); ?>
+				</div>
+				<div class="block2 floatRight">
+					<?php //$this->widget('application.components.ContactsWidget'); ?>
+					<?php //$this->widget('application.components.ConsultantWidget'); ?>					
+				</div>
+			
+				<?php //$this->widget('application.components.CurrencyWidget'); ?>
+			
+			</div>
+		</div>
 
-	<?php echo $content; ?>
+		<div class="middle">
+			<div class="width-wrap">
+			
+				<?php if(isset($this->breadcrumbs)):?>
+					<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+						'links'=>$this->breadcrumbs,
+					)); ?><!-- breadcrumbs -->
+				<?php endif?>
+			
+			
+				<?php //$this->widget('application.components.SearchAutoWidget'); ?>
+				<div class="central clearfix"><?php echo $content; ?></div>
+				<div class="content-down">
 
-	<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer">
+		<div class="width-wrap clearfix">
 
+				<div class="contacts">
+					<p class="footer-hdr">Контакты</p>
+					<p class="email">E-mail: info@s-turbo.by</p>
+					<p class="phones">Отдел продаж: <br>+375 29 530 22 99 <br>+375 44 530 22 99</p>
+					<p class="adress">Мы находимся: <br>г. Минск, АвтоМолл "Кольцо", <br>2-ой этаж, 417 павильон</p>
+					<p class="copyright">© <?php echo date('Y'); ?> <span class="site-name">S-TURBO.BY</span> Республика Беларусь</p>
+				</div>
+				
+				<div class="ur-data">
+					<p class="footer-hdr">Юридические данные</p>
+					<p class="txt">220089 г. Минск ул. Гурского 37, офис 5Н, <br>комната 18/23</p>
+					<p class="txt">
+						р/с 3012162108013 в Региональной дирекции №700<br>
+						ОАО «БПС-Сбербанк» 220035, г. Минск, пр-т Машерова, 80, код 369<br>
+						УНП 192025656
+					</p>
+					<p class="txt">
+						Директор Ксензов Евгений Олегович<br>
+						Дата регистрации в торговом реестре - 07.08.2013г.	
+					</p>
+				</div>
+				<div class="our-shop">
+					<p class="footer-hdr uppercase">Наш магазин</p>
+					<a href="#" class="view-map"><img src="/images/map.jpg" alt="Наш магазин" /></a>
+				</div>
+
+ 		</div>
+	</div><!-- footer -->
 
 </div><!-- page -->
-<div class="footer">
-	Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-	All Rights Reserved.<br/>
-	<?php echo Yii::powered(); ?>
-</div><!-- footer -->
-
 
 </body>
 </html>
